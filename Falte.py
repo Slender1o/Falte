09e720a8d9497f0477d910ed3807bb6e13b06afc
Slender1o <44403764+Slender1o@users.noreply.github.com>
2018-10-30 14:33:14 -0500

    Save satate
    Still editing...

--- Falte.py
+++ Falte.py
@@ -11,6 +11,7 @@
 import time
 import random
 import sys
+raceList = [' ', 'Orc', 'Human', 'Elf', 'Dwarf', '']
 positive = ["Yes","yes","Sure","sure","ok","OK","Ok","oK",
             "Okay","okay","Y","y","K","k","KK","kk","Kk","kK"]
 def wait(wait,NS):
@@ -18,16 +19,12 @@ def wait(wait,NS):
     if NS == False:
         print ("\n" * 64)
 
-def dprint(s):
+def dprint(s,ND):
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(0.04)
     print("\n\n")
-
-def dinput(dinput):
-    dprint(dinput)
-    uinput = input("\n\n>")
 
 def raceI(WTR,CLTHSM,CLTHSF,RCE,INTRO):
     WTR = str(WTR)
@@ -37,9 +34,10 @@ def raceI(WTR,CLTHSM,CLTHSF,RCE,INTRO):
     dprint(INTRO+RCE[2]+" .")
     while urmom == 'strait':
         dprint(" You can't help but feel nauseous while in the "+WTR+" of water.\n")
-        dinput("Would you like to get out?")
-        response = uinput
-        if response in positive[]:
+        dprint("Would you like to get out?")
+        response = input("\n\n>")
+    
+        if response in positive:
             dprint("You get out of the water. ")
             wait(3,True)
             if  gender == 'Female':
@@ -54,49 +52,49 @@ def raceI(WTR,CLTHSM,CLTHSF,RCE,INTRO):
     
 #main intro
                 
-    print ("\n" * 64)
+print ("\n" * 64)
 
-    dprint ("You are surrounded in darkness. Everything that you see is dark and confusing.")
+dprint ("You are surrounded in darkness. Everything that you see is dark and confusing.")
 
-    wait(6,True)
+wait(6,True)
 
-    dprint("A warm light surrounds you and you hear a voice:")
+dprint("A warm light surrounds you and you hear a voice:")
 
-    wait(3,True)
+wait(3,True)
 
-    dinput("Who are you, Adventurer? What is your proper name?",Name)
-    Name = uinput
+dprint("Who are you, Adventurer? What is your proper name?")
+Name = input("\n\n>")
 
-    dinput("..and your nickname, " + Name + "?")
-    nickName = uinput
+wait(3,True)
+print ("\n\n")
+dprint("..and your nickname, " + Name + "?")
+nickName = input("\n\n>")
 
-    wait(1,True)
+wait(1,True)
 
-    dprint(Name + "...that is a good, strong name. ")
+dprint(Name + "...that is a good, strong name. ")
 
-    dprint("You have a choice, " + nickName + ". You can become the fabled hero of Falte or you can die in the process.")
+dprint("You have a choice, " + nickName + ". You can become the fabled hero of Falte or you can die in the process.")
 
-    dprint(" I will now transport you into the country of Falte...\n")
+dprint(" I will now transport you into the country of Falte...\n")
 
-    wait(10,False)
+wait(10,False)
 
-    dprint("You wake up in a pool of soothing water. Your memory is hazy, at best, and you can't seem to figure out where you are.")
+dprint("You wake up in a pool of soothing water. Your memory is hazy, at best, and you can't seem to figure out where you are.")
 
-    dprint(" As you look around, you can't seem to figure out what your body looks like. You try to remember...")
+dprint(" As you look around, you can't seem to figure out what your body looks like. You concentrate...")
 
-    wait(3,False)
+wait(3,False)
 
-    dprint("As you inspect what little you can see of your body, you remember that you are...")
-    dinput("What is your gender?")
-    gender = uinput
+dprint("As you inspect what little you can see of your body, you remember that you are...")
+dprint("What is your gender?")
+gender = input("\n\n>")
 
-
-
-if (gender == 'Female'):
+if gender == 'Female':
     dprint("You remember your feminine charms and whims.")
     wait(2,True)
     dprint("You are a woman.")
-if (gender == 'Male'):
+if gender == 'Male':
     dprint("You remember your manly muscles and all your feats of strength.")
     wait(2,True)
     dprint("You are a man.")
@@ -104,25 +102,19 @@ if (gender == 'Male'):
 wait(6,False)
 
 #pick your poison
-    raceList = [' ', 'Orc', 'Human', 'Elf', 'Dwarf', '']
 
-    for i in range(1, 6):
+for i in range(1, 6):
+  dprint(raceList[i], end="")
+    
+dprint("What is your race, " + Name + "?\n")
+race = input("\n\n>")
+print ("\n\n")
+dprint("Yes. That's right. You're a(n) " + race + ".")
 
-        print(raceList[i])
+wait(6,False)
 
-        print("\n")
-        
+if race == 'Elf':
 
-        
+    raceI("pool","There is a tunic and a pair of boots.","There is a dress, a hair clip and a pair of boots.",
 
-    race = input("What is your race, " + Name + "?\n")
-
-    dprint("Yes. That's right. You're a(n) " + race + ".")
-
-    wait(6,False)
-
-    if race == 'Elf':
-
-        raceI("pool","There is a tunic and a pair of boots.","There is a dress, a hair clip and a pair of boots.",
-
-            ["Elf","elf","Elven","elven"],"You look around and see that you are in a tent with guards standing outside. They are ")
+        ["Elf","elf","Elven","elven"],"You look around and see that you are in a tent with guards standing outside. They are ")
